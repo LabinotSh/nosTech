eedb2765a4ead40872d2bfe62d269c1222953dcc
NarentHub <53078891+Narent-hub@users.noreply.github.com>
2020-10-20 09:26:21 +0200

    minor changes

--- nos-tech/src/components/Contact.js
+++ nos-tech/src/components/Contact.js
@@ -9,46 +9,56 @@ function Contact() {
                <form>
                    <h1>Got any concern?</h1>
                    <h2>Feel free to contact us!</h2>
-                   <label className="custom-field">
-                   <input 
-                        required
-                        type = "text"
-                        name = "name"/>
-                    <span className="placeholder">Name</span>        
-                    </label>
+                   <div class="form-group">
+                        <label for="exampleInputName">Name </label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="exampleInputName"
+                            placeholder="First Name"/>
+                    </div>
                     
-                    <label className="custom-field">
-                    <input
-                        required
-                        type = "text"
-                        name = "surname"/>
-                    <span className="placeholder">Surname</span>        
-                    </label>
+                    <div class="form-group">
+                        <label for="exampleInputName">Surame </label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="exampleInputName"
+                            placeholder="Surname"/>
+                    </div>
 
-                    <label className="custom-field">
-                    <input
-                        required
-                        type = "email"
-                        name = "email"/>
-                    <span className="placeholder">E-Mail</span>        
-                    </label>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input 
+                            type="email" 
+                            class="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp" 
+                            placeholder="Enter email"/>
+                    </div>
 
-                    <label className="custom-field">
-                    <input
-                        required
-                        type = "text"
-                        name = "subject"/>
-                    <span className="placeholder">Subject</span>        
-                    </label>
+                    <div class="form-group">
+                        <label for="exampleInputSubject">Subject </label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="exampleInputSubject"
+                            placeholder="Subject"/>
+                    </div>
 
-                    <label className="custom-field">
-                    <input
-                    className = "placeholder11  "
-                        required
-                        type = "text"
-                        name = "message"/>
-                    <span><textarea className="placeholder placeholder1" >Message</textarea></span>        
-                    </label>
+                    <div class="form-group">
+                        <label for="exampleInputName">Your Message/Question </label>
+                        <textarea 
+                            type="text" 
+                            class="form-control" 
+                            id="exampleInputMesage"
+                            placeholder="Message"/>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your informations with anyone else.</small>
+                    </div>
+
+                    <button type="submit" label="send" className="btn btn-primary pseudoBtn">
+                        Send
+                    </button>
                </form>
            </div>
         </div>
