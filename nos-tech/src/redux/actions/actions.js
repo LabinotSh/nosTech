23c6ef9b78495eefd42287555ccf711a1b6c6d16
NarentHub <53078891+Narent-hub@users.noreply.github.com>
2020-10-30 10:53:11 +0100

    merge conflicts

--- nos-tech/src/redux/actions/actions.js
+++ nos-tech/src/redux/actions/actions.js
@@ -1,22 +1,22 @@
-//Ex..your order = actions
+// Ex..your order = actions
 
-//krejt actions 
+// krejt actions
 
-export const REGISTER = "REGISTER"
-export const SIGNIN = "SIGN_IN";
+// export const REGISTER = "REGISTER"
+// export const SIGNIN = "SIGN_IN";
 
-export const Signin = (username, password) => (
-    {   
-        payload : SIGNIN,
-        username : username,
-        password : password
-    
-})
+// export const Signin = (username, password) => (
+//     {
+//         payload : SIGNIN,
+//         username : username,
+//         password : password
 
-export const Register = (email, password) => (
-    {
-        payload : REGISTER,
-        email : email,
-        password : password
-    }
-)
+// })
+
+// export const Register = (email, password) => (
+//     {
+//         payload : REGISTER,
+//         email : email,
+//         password : password
+//     }
+// )
--- nos-tech/src/redux/reducers/register.js
+++ nos-tech/src/redux/reducers/register.js
@@ -1,24 +1,24 @@
-import store from '../../Store';
-import { REGISTER, SIGNIN } from '../actions/actions' 
+// import store from '../../Store';
+// import { REGISTER, SIGNIN } from '../actions/actions'
 
-const initialState = {
-    email : "",
-    username : "",
-    password : "",
-    loading : false
-}
+// const initialState = {
+//     email : "",
+//     username : "",
+//     password : "",
+//     loading : false
+// }
 
-const registerReducer = ( state = initialState, action) =>  {
-    switch(action.type){
-        case REGISTER:
-            return {
-                ...state,
-                email : action.email,
-                password : action.password
-            };
-        default: 
-            return state
-    }
-};
+// const registerReducer = ( state = initialState, action) =>  {
+//     switch(action.type){
+//         case REGISTER:
+//             return {
+//                 ...state,
+//                 email : action.email,
+//                 password : action.password
+//             };
+//         default:
+//             return state
+//     }
+// };
 
-export default registerReducer
+// export default registerReducer
