1cc062f367fa9bc6023cc93edc2528beb792adae
Rilind Ramushi <rilind.ramushi@gmail.com>
2020-11-04 23:04:19 +0100

    added footer component structure,style,responsive

--- nos-tech/src/components/footer-list/FooterList.js
+++ nos-tech/src/components/footer-list/FooterList.js
@@ -17,35 +17,50 @@ function FooterList() {
       eight: "Register",
     },
     {
-      title: "Get in Touch",
-      one: "232 Lovatt Crescent, Kensington",
-      two: "(022) 2967-261",
-      three: "nostech@contact.info",
+      title: "Content",
+      one: "Home",
+      two: "Courses",
+      three: "Articles",
+      four: "Forum",
+      five: "About Us",
+      six: "Contact",
+      seven: "Login",
+      eight: "Register",
     },
+    // {
+    //   title: "Get in Touch",
+    //   one: "232 Lovatt Crescent, Kensington",
+    //   two: "(022) 2967-261",
+    //   three: "nostech@contact.info",
+    // },
   ];
 
   const renderFooterList = (li, index) => {
     return (
       <ul key={index}>
-        <Media className="footer-list">{li.title}</Media>
-        <Media className="footer-list">
-          <Link>{li.one}</Link>
-        </Media>
-        <Media className="footer-list">
-          <Link>{li.two}</Link>
-        </Media>
-        <Media className="footer-list">
-          <Link>{li.three}</Link>
-        </Media>
-        <Media className="footer-list">
-          <Link>{li.four}</Link>
-        </Media>
-        <Media className="footer-list">
-          <Link>{li.five}</Link>
-        </Media>
-        <Media className="footer-list">
-          <Link>{li.six}</Link>
-        </Media>
+        <Media className="list-title">{li.title}</Media>
+
+        <div className="list-item-container">
+          <Media className="list-item">
+            <Link to="/home" className="list-item-link">{li.one}</Link>
+          </Media>
+          <Media className="list-item">
+            <Link to="/courses" className="list-item-link">{li.two}</Link>
+          </Media>
+          <Media className="list-item">
+            <Link to="/articles" className="list-item-link">{li.three}</Link>
+          </Media>
+          <Media className="list-item">
+            <Link to="/forum" className="list-item-link">{li.four}</Link>
+          </Media>
+          <Media className="list-item">
+            <Link to="/about-us" className="list-item-link">{li.five}</Link>
+          </Media>
+          <Media className="list-item">
+            <Link to="/contact" className="list-item-link">{li.six}</Link>
+          </Media>
+        </div>
+
       </ul>
     );
   };
--- nos-tech/src/components/footer/Footer.js
+++ nos-tech/src/components/footer/Footer.js
@@ -1,72 +1,37 @@
 import React from "react";
 import "./footer.css";
 import FooterList from "../footer-list/FooterList";
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 
 const Footer = () => {
-  return (
-    <div className="footeri">
-      <FooterList />
-      <div className="row">
-        <div className="container-1">
-          <div className="col-2">
-            <div className="resources">
-              <h1>Resources</h1>
-              <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a piece of classical Latin literature from
-                45 BC, making it over 2000 years old.
-              </p>
+    return (
+        <div className="footeri">
+            <div className="container">
+                <div className="footer-content">
+
+                    <div className="the-list">
+                        <FooterList />
+                    </div>
+
+                    <div className="subscription">
+                        <p>
+                            <b>Subscribe</b> to stay up to date with the latest from nosTech:
+                        </p>
+
+                        <form>
+                            <input className="text-box" type="text" placeholder="Your email here..."></input>
+                        </form>
+
+                        <div>
+                            <Button className="submit-btn">
+                                Submit
+                            </Button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <div className="vl"></div>
-
-          <div className="col-2">
-            <div className="services">
-              <h1>Services</h1>
-              <ul>
-                <li> Courses</li>
-                <li> Private lessons</li>
-                <li> Tutorials</li>
-                <li> Online community</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="vl"></div>
-
-          <div className="col-2">
-            <div className="contact">
-              <h1>Get in Touch</h1>
-              <ul>
-                <li>nosTech.edu</li>
-                <li>nostech@gmail.com</li>
-                <li>04923^23\|/</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="vl"></div>
-
-          <div className="col-6">
-            <div className="subscription">
-              <p>
-                <b>Subscribe</b> to stay up to date with the latest from
-                nosTech:
-              </p>
-
-              <form>
-                <input type="text" placeholder="Your email here..."></input>
-              </form>
-              <div className="submit">
-                <button>Submit</button>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Footer;
