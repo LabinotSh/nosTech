728cb47a195135c8c31b664492a0d96bc4a3e7cb
Rilind Ramushi <rilind.ramushi@gmail.com>
2020-11-06 09:06:52 +0100

    Updated footer responsivenes

--- nos-tech/src/components/footer/Footer.js
+++ nos-tech/src/components/footer/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
                         </p>
 
                         <form>
-                            <input className="text-box" type="text" placeholder="Your email here..."></input>
+                            <input className="text-input-box" type="text" placeholder="Your email here..."></input>
                         </form>
 
                         <div>
