09d8456916c00f7fa43a2c7ea48ae5d3a550470e
NarentHub <53078891+Narent-hub@users.noreply.github.com>
2020-11-09 11:01:53 +0100

    sending email with nodemailer finished

--- server/src/api/user.js
+++ server/src/api/user.js
@@ -1,152 +1,194 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const schema = require('../middleware/validation');
-const {authorize} = require('../middleware/authorize');
-const {generateAccessToken, generateRefreshToken, verify} = require('../middleware/authToken');
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const schema = require('../middleware/validation')
+const { authorize } = require('../middleware/authorize')
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  verify,
+} = require('../middleware/authToken')
 
-const {refreshTokens} = require('../data/refreshTokens');
-const User = require('../models/User');
+const { refreshTokens } = require('../data/refreshTokens')
+const User = require('../models/User')
 
-
+// const emailTemplate = require('../templates/email')
+const { confirmEmail, contactEmail } = require('../../nodemailer/email')
 
 //Get All the users
-router.get('/', async (req,res) =>{
-    try{
-        const users = await User.find();
-        res.header('Content-Range', `course 0-10/${users.length}`);
-        res.json(users);
-    }catch(err){
-        console.log({
-            message:err
-        });
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.header('Content-Range', `course 0-10/${users.length}`)
+    res.json(users)
+  } catch (err) {
+    console.log({
+      message: err,
+    })
+  }
+})
+
+router.post('/contact', async (req, res) => {
+  try {
+    const { name, sname, subject, email, message } = req.body
+
+    const sent = await contactEmail(name, sname, email, subject, message)
+    res.json({ msg: sent })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.post('/register', async (req, res) => {
+  const { error } = schema.registrationValidate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  //Check if that email already exists
+  const emailExists = await User.findOne({
+    email: req.body.email,
+  })
+  if (emailExists) return res.status(400).send('Email already exists!')
+
+  //Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+  try {
+    const newUser = new User({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: hashedPassword,
+      role: req.body.role,
+      username: req.body.username,
+    })
+
+    // const savedUser = await newUser.save();
+    // res.send({user: savedUser});
+    // const savedUser = await newUser.save();
+    const savedUser = newUser
+      .save()
+      .then((newUser) => {
+        confirmEmail(newUser.email, emailTemplate.confirm(newUser._id))
+      })
+      .then(() =>
+        res.json({ msg: 'Email sent, please check your inbox to verify it!' })
+      )
+      .catch((err) => console.log(err))
+
+    //const emailed =  await sendEmail(savedUser.email, emailTemplate.confirm(savedUser._id));
+    // .then(() => res.json({msg: 'Please confirm your email!'}))
+    // .catch(err => console.log(err));
+
+    if (emailExists && !emailExists.confirmed) {
+      await sendEmail(
+        emailExists.email,
+        emailTemplate.confirm(emailExists._id)
+      ).then(() => res.json({ msg: 'An email message was resent to you!' }))
     }
-});
-
-router.post('/register', async (req,res) => {
-
-    const {error} = schema.registrationValidate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    //Check if that email already exists
-    const emailExists = await User.findOne({
-        email: req.body.email
-    });
-    if(emailExists) return res.status(400).send('Email already exists!');
-
-    //Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password,salt);
-
-    try {
-        const newUser = new User({
-            name: req.body.name,
-            surname: req.body.surname,
-            email: req.body.email,
-            password: hashedPassword,
-            role: req.body.role,
-            username: req.body.username
-        })
-
-        const savedUser = await newUser.save();
-        res.send({user: savedUser});
-    } catch (error) {
-        console.log(error);
-    }
-});
+    // res.send({user: savedUser, email: emailed});
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 //login
-router.post('/login', async (req,res) => {
+router.post('/login', async (req, res) => {
+  const { error } = schema.loginValidate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-    const {error} = schema.loginValidate(req.body);
-     if(error) return res.status(400).send(error.details[0].message);
+  const user = await User.findOne({
+    username: req.body.username,
+  })
 
-    const user = await User.findOne({
-           username: req.body.username
-   });
-   
-   if(!user) return res.status(400).send('Username or password wrong!');
+  if (!user) return res.status(400).send('Username or password wrong!')
 
-   const validPassword = await bcrypt.compare(req.body.password, user.password);
-   if(!validPassword) return res.status(400).send('Invalid Password!');
+  const validPassword = await bcrypt.compare(req.body.password, user.password)
+  if (!validPassword) return res.status(400).send('Invalid Password!')
 
-   const accessToken = generateAccessToken(user);
-   const refreshToken = generateRefreshToken(user);
-   user.tokens = refreshTokens; 
-   user.tokens.push(refreshToken);
-   
-//    console.log('dd : ' + user.tokens);
-//    console.log('REFFF ' + refreshToken);
+  const accessToken = generateAccessToken(user)
+  const refreshToken = generateRefreshToken(user)
+  user.tokens = refreshTokens
+  user.tokens.push(refreshToken)
 
-   res.cookie("jwt", accessToken, {secure: false, httpOnly: false})
+  //    console.log('dd : ' + user.tokens);
+  //    console.log('REFFF ' + refreshToken);
 
-//    res.header('auth-token', accessToken).send( {
-//        refreshToken: refreshToken
-//    });
+  res.cookie('jwt', accessToken, { secure: false, httpOnly: false })
 
-   res.send({
+  //    res.header('auth-token', accessToken).send( {
+  //        refreshToken: refreshToken
+  //    });
+
+  res.send({
     message: 'authentication done ',
     token: accessToken,
     refreshToken: refreshToken,
-    user: user
-  });
-   
-});
+    user: user,
+  })
+})
 
 //Get the new token from the generated refresh token
-router.post('/token', async (req,res) => {
-    const refreshToken = req.body.token;
-    if(refreshToken == null) return res.sendStatus(401)
-    if(!tokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-       if(err) return res.sendStatus(403)
-       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+router.post('/token', async (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!tokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 
-       res.cookie("jwt", accessToken, {secure: false, httpOnly: false})
+    res.cookie('jwt', accessToken, { secure: false, httpOnly: false })
 
-       res.json({ accessToken: accessToken })
-    })
-});
+    res.json({ accessToken: accessToken })
+  })
+})
 
 //Get one user by id
-router.get('/:userId', async(req,res) => {
-    try {
-        const id = req.params.userId;
-        const user = await User.findOne({_id:id});
-        res.json(user);
-        
-    } catch (error) {
-        console.error(error);
-    }
-});
-
+router.get('/:userId', async (req, res) => {
+  try {
+    const id = req.params.userId
+    const user = await User.findOne({ _id: id })
+    res.json(user)
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 //Delete a user by id
-router.delete('/:userId', async (req,res) => {
-    const id = req.params.userId;
-    try {
-        const deletedUser = await User.deleteOne({
-            _id: id
-        })
-        res.send(deletedUser);
-    } catch (error) {
-        console.error(error);
-    }
-});
-
+router.delete('/:userId', async (req, res) => {
+  const id = req.params.userId
+  try {
+    const deletedUser = await User.deleteOne({
+      _id: id,
+    })
+    res.send(deletedUser)
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 //Update a user by id
-router.patch('/:userId', async (req,res) => {
-    try {
-        const id = req.params.userId;
-        const updated = await User.updateOne({
-            _id: id
-        })
-        res.json(updated);
-    } catch (error) {
-        console.error(error);
-    }
-});
+// Update a user by id
+router.patch('/:userId', async (req, res) => {
+  try {
+    const id = req.params.userId
+    const updated = await User.updateOne({
+      _id: id,
+    })
+    res.json(updated)
+  } catch (error) {
+    console.error(error)
+  }
+})
 
-module.exports = router;
+// router.put('/:id', async(req,res) => {
+//     const id = req.params.id;
+
+//     const userup = await User.findByIdAndUpdate(id, {
+//         confirmed:true
+//     });
+//     res.json('updated ' + userup);
+// })
+
+module.exports = router
