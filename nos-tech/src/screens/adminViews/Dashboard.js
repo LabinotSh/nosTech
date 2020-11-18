c74ffa3353c1d9999281056cd57156d2e75e1aa2
Besart Haxhiu <bh40793@ubt-uni.net>
2020-11-18 16:02:24 +0100

    Dashboard Panel/Pagination/Router Changes

---
+++ nos-tech/src/screens/adminViews/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Panel from '../../components/panel/Panel'
+
+const Dashboard = () => {
+    return(
+        <div>
+        <Panel />
+        <h1>Dashboard</h1>
+        </div>
+    )
+}
+
+export default Dashboard
